441e3b6506513fc14e79fa63b4a3ff59e7412f4d
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-09-24 01:14:36 -0700

    Add files via upload

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -70,4 +70,10 @@ public class CameraController : MonoBehaviour
         }
         
     }
+
+    public void SetAsSpectator()
+    {
+        isSpectator = true;
+        transform.parent = null;
+    }
 }
